6875ab584068273795d6bcd283153be75827c866
sduchenko <sduchenko@moysklad.ru>
2024-05-27 16:12:33 +0300

    hw-2 реализовано редактирование и добавление контрагентов

--- src/app/app.js
+++ src/app/app.js
@@ -1,20 +1,47 @@
 import html from "./app.html"
-import dataJson from "./data.json"
+
 import './app.css'
 
 import Table from './contragent/table'
-
-let data = [...dataJson];
+import Modal from './contragent/modal'
+import ContragentRepository from "./contragent/data";
 
 const rootElement = document.getElementById('root');
 rootElement.innerHTML = html;
-
 const main = document.getElementById("main");
+const addButton = document.getElementById("add-button");
 
+const contragentRepositort = new ContragentRepository();
+
+let modal = new Modal();
 let table = new Table();
 
-table.mount(main);
-table.onDblClick(() => {
-    alert("Clicked!");
+function refresh() {
+    contragentRepositort.load()
+        .then(data => table.setData(data));
+}
+
+function addNew() {
+    contragentRepositort.create()
+        .then(item => modal.edit(item))
+        .then(edited => {
+            return contragentRepositort.save(edited)
+        }).then(() => refresh());
+}
+
+table.onDblClick((item) => {
+    modal.edit(item)
+        .then(edited => {
+            return contragentRepositort.save(edited)
+        }).then(() => refresh());
 });
-table.setData(data);
+
+addButton.onclick = (e) => {
+    e.preventDefault();
+    addNew();
+};
+
+table.mount(main);
+modal.mount(main);
+
+refresh();
